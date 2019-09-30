12a50f2d5624b57565094ca35b7cf3562157ae45
ben-abbott <36866524+ben-abbott@users.noreply.github.com>
2019-09-30 10:51:11 +0100

    Initial commit

---
+++ controllers/handleImage.js
@@ -0,0 +1,29 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: '1c46ebe70ea74a5f992a6429b6ff185e'
+});
+
+const handleApiCall = (req, res) => {
+	app.models
+		.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+		.then(data => {
+			res.json(data);
+		})
+		.catch(err => res.status(400).json('unable to connect with api'))
+}
+
+const handleImage = (req, res, db) => {
+	const { id } = req.body;
+	db('users').where('id', '=', id)
+	.increment('entries', 1)
+	.returning('entries')
+	.then(entries => {
+		res.json(entries[0]);
+	})
+	.catch(err => res.status(400).json('Unable to get entries'))
+}
+
+module.exports = {
+	handleImage
+}
